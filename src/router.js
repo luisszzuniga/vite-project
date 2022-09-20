dd2380050e7533869032cd90e8d79567a43898b4
Luisszzuniga <l.zuniga@webandcow.com>
2022-09-20 15:10:53 +0200

    Add bootstrap + vue-router

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import * as VueRouter from 'vue-router'
+
+const Home = () => import('./pages/Home.vue')
+const Login = () => import('./pages/Login.vue')
+const ChooseTemplate = () => import('./pages/ChooseTemplate.vue')
+const ChooseServer = () => import('./pages/ChooseServer.vue')
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/login', component: Login },
+  { path: '/choose-template', component: ChooseTemplate },
+  { path: '/choose-server', component: ChooseServer },
+]
+
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+})
+
+export default router;
