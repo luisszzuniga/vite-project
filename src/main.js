95fa36f9a1bb4bf1704526bb6df344ace0f74e07
Luisszzuniga <l.zuniga@webandcow.com>
2022-09-20 15:11:25 +0200

    4 new pages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router.js'
 
-createApp(App).mount('#app')
+import Bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+createApp(App).use(router).use(Bootstrap).mount('#app');
---
+++ src/pages/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>login</h1>
+    <h2>here</h2>
+    <p>{{test}}</p>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                test: 'null'
+            }
+        }
+    }
+</script>
